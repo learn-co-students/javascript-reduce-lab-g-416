c3a8725a295adebd771172c677bb5097f7595b42
Thomas Tuts <thomas@thomastuts.com>
2016-07-19 17:18:17 +0200

    Add content and tests for exercises (#2)
    * Add content and tests for exercises
    
    * Add missing line for example monologue output

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const app = "I don't do much."
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+const monologueLines = [
+  'Who are you talking to right now?',
+  'Who is it you think you see?',
+  'Do you know how much I make a year?',
+  'I mean, even if I told you, you wouldn\'t believe it.',
+  'Do you know what would happen if I suddenly decided to stop going into work?',
+  'A business big enough that it could be listed on the NASDAQ goes belly up.',
+  'Disappears!',
+  'It ceases to exist without me.',
+  'No, you clearly don\'t know who you\'re talking to, so let me clue you in.',
+  'I am not in danger, Skyler.',
+  'I am the danger.',
+  'A guy opens his door and gets shot and you think that of me?',
+  'No.',
+  'I am the one who knocks!'
+];
