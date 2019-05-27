9dcf998065eb10d65613d5c30d90194c80b91b7d
github username <github email address>
2019-05-26 21:07:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { cursorTo } from "readline";
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 const monologueLines = [
@@ -16,3 +18,17 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+
+const totalBatteries = batteryBatches.reduce((accumulator, currentVal) => accumulator + currentVal, 0);
+
+const wordCountMap = monologueLines.reduce((map, sentence) => {
+  const wordCount = sentence.split(" ").length;
+
+  if(!map[wordCount]){
+    map[wordCount] = 0;
+  }
+  map[wordCount]++
+
+  return map;
+}, {});
