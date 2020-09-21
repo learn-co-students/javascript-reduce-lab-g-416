e8d4d8c1f8f869a4b25ca6e6bb49de01c3d02654
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-09-21 15:51:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+let totalBatteries= batteryBatches.reduce(function(accumulator, value){
+  return accumulator + value
+})
+
+console.log(totalBatteries)
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +22,17 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+const wordCountMap = monologueLines.reduce((map, sentence) => {
+  const wordCount = sentence.split(' ').length;
+
+  if (!map[wordCount]) {
+    map[wordCount] = 0;
+  }
+
+  map[wordCount]++;
+
+  return map;
+}, {});
+
+console.log(wordCountMap)
