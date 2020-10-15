9de142049e260f37279272632a87f7382edcbbdf
dmarcr1997 <dmarcr1997@gmail.com>
2020-10-15 12:22:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce((a,b) => a+b, 0)
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +18,15 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+const wordCountMap = monologueLines.reduce((map, sentence) => {
+  let wordCount = sentence.split(' ').length;
+
+  if(!map[wordCount]) {
+    map[wordCount] = 0;
+  }
+
+  map[wordCount]++;
+  return map;
+}, {});
+
